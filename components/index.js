2d4c43d81c1937c828c04d2b9d7fe65e6065a8d8
Aditya <adiroychaudhuri@gmail.com>
2026-04-25 23:55:59 +0530

    added the export for frequent questions and CTA component

--- components/index.js
+++ components/index.js
@@ -3,3 +3,5 @@ export { default as HeroSection } from "./HeroSection";
 export { default as Stats } from "./Stats";
 export { default as Workings } from "./Workings";
 export { default as Testimontials } from "./Testimonials"
+export { default as FrequentQuestions } from './FrequentQuestions'
+export { default as CTA } from './CTA'
